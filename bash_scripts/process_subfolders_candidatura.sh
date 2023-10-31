50249d38ec16b65af51cc68486c98d94ee88e8b8
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-10-31 11:03:14 +0100

    Create process_subfolders_candidatura.sh

---
+++ bash_scripts/process_subfolders_candidatura.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Markdown file to store the structure
+markdown_file="prova.md"
+
+# Function to process a directory
+process_directory() {
+  local dir="$1"
+  local indent="$2"
+
+  # Append the directory name with proper indentation
+  echo "- $indent$(basename "$dir")" >> "$markdown_file"
+
+  # Enter the directory
+  cd "$dir" || exit
+
+  # Loop through the files in the directory
+  for file in *; do
+    if [ -f "$file" ]; then
+      # Append file names with proper indentation
+      echo "  - $indent$file" >> "$markdown_file"
+    fi
+  done
+
+  # Recursively process subdirectories
+  for sub_dir in */; do
+    process_directory "$sub_dir" "$indent  "
+  done
+
+  # Return to the parent directory
+  cd ..
+}
+
+# Start processing the root directory
+root_directory="./docs/Candidatura"
+echo "# File Structure" > "$markdown_file"
+process_directory "$root_directory" ""
+
+echo "File structure has been generated and saved in $markdown_file."

5e4e4f159b1884a233cfebbe6b01bce4213ab6f1
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-10-31 19:53:43 +0100

    Update update_candidatura.sh

--- bash_scripts/update_candidatura.sh
+++ bash_scripts/update_candidatura.sh
@@ -51,10 +51,10 @@ function add_folder_contents {
 # Esegui la funzione ricorsiva sulla cartella principale
 add_folder_contents . ""
 
-mv "../$markdown_file" ../_pages/
+mv "../$markdown_file" ../../
 # Esegui i comandi Git
 git config user.email "actions@github.com"
 git config user.name "GitHub Actions"
-git add "../_pages/$markdown_file"
+git add "../../$markdown_file"
 git commit -m "Update file links"
 git push

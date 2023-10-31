9def1297ba40c300734ec3c23c8eb0cf089604ba
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-10-31 11:50:03 +0100

    Update process_subfolders_candidatura.sh

--- bash_scripts/process_subfolders_candidatura.sh
+++ bash_scripts/process_subfolders_candidatura.sh
@@ -2,7 +2,7 @@
 
 # Markdown file to store the structure
 markdown_file="prova.md"
-> "./docs/prova.md"
+
 # Function to process a directory
 process_directory() {
   local dir="$1"
@@ -11,29 +11,34 @@ process_directory() {
   # Append the directory name with proper indentation
   echo "- $indent$(basename "$dir")" >> "$markdown_file"
 
-  # Enter the directory
-  cd "$dir" || return
+  # Use a subshell to isolate directory changes
+  (
+    cd "$dir" || return
 
-  # Loop through the files in the directory
-  for file in *; do
-    if [ -f "$file" ]; then
-      # Append file names with proper indentation
-      echo "  - $indent$file" >> "$markdown_file"
-    fi
-  done
+    # Loop through the files in the directory
+    for file in *; do
+      if [ -f "$file" ]; then
+        # Append file names with proper indentation
+        echo "  - $indent$file" >> "$markdown_file"
+      fi
+    done
 
-  # Recursively process subdirectories
-  for sub_dir in */; do
-    echo $sub_dir
-    process_directory "$sub_dir" "$indent  "
-  done
+    # Recursively process subdirectories
+    for sub_dir in */; do
+      echo $sub_dir
+      process_directory "$sub_dir" "$indent  "
+    done
+  )
 
-  # Return to the parent directory
-  cd ..
+  # No need to return to the parent directory here
 }
 
 # Start processing the root directory
 root_directory="./docs/Candidatura"
+
+# Create the Markdown file or clear it if it exists
+touch "${root_directory}/$markdown_file"
+
 echo "# File Structure" > "${root_directory}/$markdown_file"
 process_directory "$root_directory" ""
 

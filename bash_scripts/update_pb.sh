c93ec34dfe6e293d3f75d00a7895bbb948bb991b
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-10-31 18:47:21 +0100

    Update update_pb.sh

--- bash_scripts/update_pb.sh
+++ bash_scripts/update_pb.sh
@@ -8,10 +8,8 @@ cd "$folder_path" || exit
 
 echo "---" >> "../$markdown_file"
 echo "layout: default" >> "../$markdown_file"
-echo "title: Candidatura" >> "../$markdown_file"
+echo "title: Product Baseline" >> "../$markdown_file"
 echo "---" >> "../$markdown_file"
-
-echo "# Product Baseline" >> "../$markdown_file"
 
 # Funzione ricorsiva per aggiungere il nome delle sottocartelle e il loro contenuto
 function add_folder_contents {

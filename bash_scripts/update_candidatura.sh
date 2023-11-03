f5085c9a0fdfccaa1af4734f57164101e7bc4e1f
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-11-03 14:50:15 +0100

    Update update_candidatura.sh

--- bash_scripts/update_candidatura.sh
+++ bash_scripts/update_candidatura.sh
@@ -60,6 +60,10 @@ git config user.email "actions@github.com"
 git config user.name "GitHub Actions"
 git add "$markdown_file"
 git commit -m "Update file links"
-git config pull.rebase false
+
+cd ..
+cd ..
+dir
+
 git pull origin try-change-path
 git push -u origin try-change-path 
